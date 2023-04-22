4988256cc5877edf776042cde16c9984b12b1a6e
Peter Milne <milnepe@gmail.com>
2023-04-22 17:58:56 +0100

    Bump version

--- FloodMagnetController/FloodMagnetController.ino
+++ FloodMagnetController/FloodMagnetController.ino
@@ -35,7 +35,7 @@ If you make use of this data please acknowledge this with the following attribut
 
 enum mode mymode = STD_MODE;
 
-const char* soft_version = "0.2.1";
+const char* soft_version = "0.3.0";
 
 FloodAPI myFloodAPI = FloodAPI();
 
