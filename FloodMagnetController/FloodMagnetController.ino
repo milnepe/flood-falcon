fc9fc4f676efc962e4111e339f06269d901dd791
Peter Milne <milnepe@gmail.com>
2023-05-01 14:46:19 +0100

    Bump firmware & schematic versions

--- FloodMagnetController/FloodMagnetController.ino
+++ FloodMagnetController/FloodMagnetController.ino
@@ -35,7 +35,7 @@ If you make use of this data please acknowledge this with the following attribut
 
 modes mode = STD_MODE;
 
-const char* soft_version = "0.3.1";
+const char* soft_version = "0.4.0";
 
 FloodAPI myFloodAPI = FloodAPI();
 
